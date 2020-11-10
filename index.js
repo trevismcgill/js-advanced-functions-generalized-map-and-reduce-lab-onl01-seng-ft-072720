8f43cf81b2cbc5fe8ecbd9a9daf5a54933788c0b
trevismcgill <trevismcgill25@gmail.com>
2020-11-10 00:27:43 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Add your functions here
+
+function map(array, cb) {
+    let r = []
+  
+    for (let i = 0; i < array.length; i++) {
+      let theElement = array[i]
+      r.push(cb(theElement))
+    }
+  
+    return r;
+  }
+  
+  function reduce(array, cb, start){
+    let r = (!!start) ? start : array[0]
+    let i = (!!start) ? 0 : 1
+  
+    for (; i < array.length; i++) {
+      r = cb(array[i], r)
+    }
+  
+    return r;
+  }
